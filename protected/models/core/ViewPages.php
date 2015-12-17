d4c9875814d3de5a93b2e118cf90bf5e63a3f8f0
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2015-12-17 15:42:35 +0700

    commit

--- protected/models/core/ViewPages.php
+++ protected/models/core/ViewPages.php
@@ -3,7 +3,7 @@
  * ViewCorePages
  * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
  * @copyright Copyright (c) 2015 Ommu Platform (ommu.co)
- * @link http://company.ommu.co
+ * @link https://github.com/oMMu/Ommu-Core
  * @contact (+62)856-299-4114
  *
  * This is the template for generating the model class of a specified table.
