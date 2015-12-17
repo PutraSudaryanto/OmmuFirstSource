7fcb3226fbc62392afedceafa81630fee027ebef
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2015-12-17 16:18:33 +0700

    zone village, commit view model

---
+++ protected/models/core/ViewZoneVillage.php
@@ -0,0 +1,200 @@
+<?php
+/**
+ * ViewZoneVillage
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2015 Ommu Platform (ommu.co)
+ * @link https://github.com/oMMu/Ommu-Core
+ * @contact (+62)856-299-4114
+ *
+ * This is the template for generating the model class of a specified table.
+ * - $this: the ModelCode object
+ * - $tableName: the table name for this class (prefix is already removed if necessary)
+ * - $modelClass: the model class name
+ * - $columns: list of table columns (name=>CDbColumnSchema)
+ * - $labels: list of attribute labels (name=>label)
+ * - $rules: list of validation rules
+ * - $relations: list of relations (name=>relation declaration)
+ *
+ * --------------------------------------------------------------------------------------
+ *
+ * This is the model class for table "_view_core_zone_village".
+ *
+ * The followings are the available columns in table '_view_core_zone_village':
+ * @property string $village_id
+ * @property string $village_name
+ * @property string $district_name
+ * @property string $city
+ * @property string $province
+ */
+class ViewZoneVillage extends CActiveRecord
+{
+	public $defaultColumns = array();
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return ViewZoneVillage the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return '_view_core_zone_village';
+	}
+
+	/**
+	 * @return string the primarykey column
+	 */
+	public function primaryKey()
+	{
+		return ['village_id'];
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('village_name', 'required'),
+			array('village_id', 'length', 'max'=>11),
+			array('village_name, district_name, city, province', 'length', 'max'=>64),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('village_id, village_name, district_name, city, province', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'village_id' => 'Village',
+			'village_name' => 'Village Name',
+			'district_name' => 'District Name',
+			'city' => 'City',
+			'province' => 'Province',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('t.village_id',strtolower($this->village_id),true);
+		$criteria->compare('t.village_name',strtolower($this->village_name),true);
+		$criteria->compare('t.district_name',strtolower($this->district_name),true);
+		$criteria->compare('t.city',strtolower($this->city),true);
+		$criteria->compare('t.province',strtolower($this->province),true);
+
+		if(!isset($_GET['ViewZoneVillage_sort']))
+			$criteria->order = 'village_id DESC';
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+			'pagination'=>array(
+				'pageSize'=>30,
+			),
+		));
+	}
+
+
+	/**
+	 * Get column for CGrid View
+	 */
+	public function getGridColumn($columns=null) {
+		if($columns !== null) {
+			foreach($columns as $val) {
+				/*
+				if(trim($val) == 'enabled') {
+					$this->defaultColumns[] = array(
+						'name'  => 'enabled',
+						'value' => '$data->enabled == 1? "Ya": "Tidak"',
+					);
+				}
+				*/
+				$this->defaultColumns[] = $val;
+			}
+		} else {
+			$this->defaultColumns[] = 'village_id';
+			$this->defaultColumns[] = 'village_name';
+			$this->defaultColumns[] = 'district_name';
+			$this->defaultColumns[] = 'city';
+			$this->defaultColumns[] = 'province';
+		}
+
+		return $this->defaultColumns;
+	}
+
+	/**
+	 * Set default columns to display
+	 */
+	protected function afterConstruct() {
+		if(count($this->defaultColumns) == 0) {
+			$this->defaultColumns[] = array(
+				'header' => 'No',
+				'value' => '$this->grid->dataProvider->pagination->currentPage*$this->grid->dataProvider->pagination->pageSize + $row+1'
+			);
+			$this->defaultColumns[] = 'village_id';
+			$this->defaultColumns[] = 'village_name';
+			$this->defaultColumns[] = 'district_name';
+			$this->defaultColumns[] = 'city';
+			$this->defaultColumns[] = 'province';
+		}
+		parent::afterConstruct();
+	}
+
+	/**
+	 * User get information
+	 */
+	public static function getInfo($id, $column=null)
+	{
+		if($column != null) {
+			$model = self::model()->findByPk($id,array(
+				'select' => $column
+			));
+			return $model->$column;
+			
+		} else {
+			$model = self::model()->findByPk($id);
+			return $model;			
+		}
+	}
+
+}
