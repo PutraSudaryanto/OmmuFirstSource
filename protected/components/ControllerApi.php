c6d7eb3a94798d45a04f29bfac55219bd8509dce
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-06-19 08:56:00 +0700

    ommu: controller api

---
+++ protected/components/ControllerApi.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * ControllerApi is the customized base controller api class.
+ * All controller api classes for this application should extend from this base class.
+ * 
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @link https://github.com/oMMu/Ommu-Core
+ * @contect (+62)856-299-4114
+ *
+ */
+
+class ControllerApi extends Controller
+{
+    /**
+     * convert array to json
+     * @param array $data
+     */
+    public function renderJson($data, $encode=true) {
+        $this->layout = false;
+        header('Content-Type: application/json');
+		if($encode)
+        	echo CJSON::encode($data);
+		else
+			echo $data;
+        Yii::app()->end();
+    }
+	
+	private function _sendResponse($status = 200, $body = '', $content_type = 'text/html') {
+		// set the status
+		$status_header = 'HTTP/1.1 ' . $status . ' ' . $this->_getStatusCodeMessage($status);
+		header($status_header);
+		// and the content type
+		header('Content-type: ' . $content_type);
+
+		// pages with body are easy
+		if ($body != '') {
+			// send the body
+			echo $body;
+		}
+		// we need to create the body if none is passed
+		else {
+			// create some body messages
+			$message = '';
+			switch ($status) {
+				case 401:
+					$message = 'You must be authorized to view this page.';
+					break;
+				case 404:
+					$message = 'The requested URL ' . $_SERVER['REQUEST_URI'] . ' was not found.';
+					break;
+				case 500:
+					$message = 'The server encountered an error processing your request.';
+					break;
+				case 501:
+					$message = 'The requested method is not implemented.';
+					break;
+			}
+
+			// servers don't always have a signature turned on
+			// (this is an apache directive "ServerSignature On")
+			$signature = ($_SERVER['SERVER_SIGNATURE'] == '') ? $_SERVER['SERVER_SOFTWARE'] . ' Server at ' . $_SERVER['SERVER_NAME'] . ' Port ' . $_SERVER['SERVER_PORT'] : $_SERVER['SERVER_SIGNATURE'];
+
+			// this should be templated in a real-world solution
+			$body = '
+	<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+	<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<title>' . $status . ' ' . $this->_getStatusCodeMessage($status) . '</title>
+	</head>
+	<body>
+		<h1>' . $this->_getStatusCodeMessage($status) . '</h1>
+		<p>' . $message . '</p>
+		<hr />
+		<address>' . $signature . '</address>
+	</body>
+	</html>';
+
+			echo $body;
+		}
+		Yii::app()->end();
+	}
+
+	private function _getStatusCodeMessage($status) {
+		// these could be stored in a .ini file and loaded
+		// via parse_ini_file()... however, this will suffice
+		// for an example
+		$codes = Array(
+			200 => 'OK',
+			400 => 'Bad Request',
+			401 => 'Unauthorized',
+			402 => 'Payment Required',
+			403 => 'Forbidden',
+			404 => 'Not Found',
+			500 => 'Internal Server Error',
+			501 => 'Not Implemented',
+		);
+		return (isset($codes[$status])) ? $codes[$status] : '';
+	}
+	
+}
---
+++ protected/ommu/MessageHelper.php
@@ -0,0 +1,15 @@
+<?php
+class MessageHelper
+{
+    public static function missingTranslation($event)
+    {
+        $body = implode("\n", array(
+                "Language: {$event->language}",
+                "Category: {$event->category}",
+                "Message: {$event->message}",
+        ));
+		print_r($event);
+		echo $body;
+    }
+}
+?>
