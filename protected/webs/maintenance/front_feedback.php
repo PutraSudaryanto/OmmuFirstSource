5ead05cccb2cfcf40ff4a0ca4d3d91faf5d495b7
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-05-20 05:34:46 +0700

    commit

--- protected/webs/maintenance/front_feedback.php
+++ protected/webs/maintenance/front_feedback.php
@@ -52,13 +52,11 @@
 						<?php echo $form->error($model,'email'); ?>
 					</div>
 				<?php } else {
-					if($user->photo_id == 0) {
-						$images = Utility::getTimThumb(Yii::app()->request->baseUrl.'/public/users/default.png', 60, 60, 1);
-					} else {
-						$images = Utility::getTimThumb(Yii::app()->request->baseUrl.'/public/users/'.$user->user_id.'/'.$user->photo->photo, 60, 60, 1);
-					}?>
+					$image = Yii::app()->request->baseUrl.'/public/users/default.png';
+					if($user->photos)
+						$image = Yii::app()->request->baseUrl.'/public/users/'.$user->user_id.'/'.$user->photos;?>
 					<div class="user-info clearfix">
-						<img src="<?php echo $images;?>" alt="<?php echo $user->photo_id != 0 ? $user->displayname: 'Ommu Platform';?>"/>
+						<img src="<?php echo Utility::getTimThumb($image, 60, 60, 1);?>" alt="<?php echo $user->photos ? $user->displayname: 'Ommu Platform';?>"/>
 						<div>
 							<h3><?php echo $user->displayname;?></h3>
 							<?php echo $user->email;?>
