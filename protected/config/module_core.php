94e7b3ef94a567689d11aabba257df8034b10024
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-11-21 17:51:46 +0700

    commit

--- protected/config/module_core.php
+++ protected/config/module_core.php
@@ -16,6 +16,9 @@ return array(
 		// uncomment the following to enable the Gii tool
 		'gii'=>array(
 			'class'     => 'system.gii.GiiModule',
+            'generatorPaths'=>array(
+                'application.libraries.gii',
+            ),
 			'password'  => '0o9i8u7y',
 		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters' => array('127.0.0.1','::1'),
