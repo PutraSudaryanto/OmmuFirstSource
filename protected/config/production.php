c5ccaa621f3b9125f262046c93830ac08315a793
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-01-07 08:31:21 +0700

    commit

--- protected/config/production.php
+++ protected/config/production.php
@@ -6,7 +6,7 @@
  * uncomment the following to define a path alias
  * Yii::setPathOfAlias('local','path/to/local-folder');
  *
- * * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
  * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2012 Ommu Platform (opensource.ommu.co)
  * @link https://github.com/ommu/ommu
