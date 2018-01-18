15b38ec12f43226f811d43ee0413df4da339847f
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-01-18 18:45:18 +0700

    add email template generate from system

---
+++ email_template.php
@@ -0,0 +1,7 @@
+{content}
+
+<p>If you have any questions please answer to this email or contact us at <a href="mailto:support@sudaryanto.id">support@sudaryanto.id</a> and <a href="callto:+628112540432">+628112540432</a></p>
+Salam,<br/>
+RXK Indonesia<br/>
+Pandes 1, Wonokromo, Kabupaten Bantul, Di Yogyakarta Indonesia<br/>
+<a href="http://www.sudaryanto.id" title="">Website</a>
