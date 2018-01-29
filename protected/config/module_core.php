f39effc554e83989badc11e417588a22f1f166ed
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-01-29 10:25:33 +0700

    register user, report and support module in core-modules

--- protected/config/module_core.php
+++ protected/config/module_core.php
@@ -23,5 +23,8 @@ return array(
 		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters' => array('127.0.0.1','::1'),
 		),
+		'users',
+		'report',
+		'support',
 	),
 );
