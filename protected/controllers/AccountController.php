a5d9c8f6e46cb6290836bf8210c25ee8b4353933
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-06 07:34:48 +0700

    move theme from function to trait

--- protected/controllers/AccountController.php
+++ protected/controllers/AccountController.php
@@ -38,7 +38,7 @@ class AccountController extends Controller
 	 */
 	public function init() 
 	{
-		$arrThemes = Utility::getCurrentTemplate('public');
+		$arrThemes = $this->currentTemplate('public');
 		Yii::app()->theme = $arrThemes['folder'];
 		$this->layout = $arrThemes['layout'];
 	}
